d0f8c3e3bbff2b1cda2cef1924188703f066b55c
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-06-08 20:34:49 +0300

    Update docker_help.bash

--- docker_help.bash
+++ docker_help.bash
@@ -5,3 +5,12 @@ sudo docker exec -it 4acda249d78a  bash
 # copy files from host into contaiter
 
 sudo docker cp postgres.py  airflow:/usr/local/airflow/dags
+
+# work with postgres
+
+sudo docker exec -it my_postgres_2 psql -U root -c "create database my_database"
+
+sudo docker cp ./dss.ddl my_postgres_2:/
+
+
+sudo  docker exec -it my_postgres_2 psql my_database -f dss.ddl
